1d383502f130f8bb4be7aed2b5dc30df4fc4b705
benjaminescobar1 <135399618+benjaminescobar1@users.noreply.github.com>
2023-11-26 15:48:49 -0800

    fixing code

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,14 +1,37 @@
 const jwt = require('jsonwebtoken');
 
-const authService = new AuthService();
-export default authService;
-
-const secret = 'mysecretssshhhhhhh';
+// set token secret and expiration date
+const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
-  signToken: function ({ email, name, _id }) {
-    const payload = { email, name, _id };
+  // function for our authenticated routes
+  authMiddleware: function ({ req }) {
+    // allows token to be sent via  req.query or headers
+    let token = req.body.token || req.query.token || req.headers.authorization;
+
+    // ["Bearer", "<tokenvalue>"]
+    if (req.headers.authorization) {
+      token = token.split(' ').pop().trim();
+    }
+
+    if (!token) {
+      return req;
+    }
+
+    // verify token and get user data out of it
+    try {
+      const { data } = jwt.verify(token, secret, { maxAge: expiration });
+      req.user = data;
+    } catch {
+      console.log('Invalid token');
+    }
+    
+    return req;
+  },
+  signToken: function ({ username, email, _id }) {
+    const payload = { username, email, _id };
+
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
 };
